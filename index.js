7bf97c6d750fe9ddd2b6e349d18e8e7503082712
Henry Okam <hnry_ugo@yahoo.com>
2022-06-20 23:43:45 +0100

    hbvkj

---
+++ index.js
@@ -0,0 +1,9 @@
+// To print out name, height and country
+
+var fullname = "Henry Ugochukwu Okam"
+var height = 1.85
+var country = "Nigeria"
+
+alert("My name is " + fullname);
+alert("I am " + height + " meters tall");
+alert("I am from " + country);
